bc76c4a622ea789bdb5dec98eb2ff61e2f3cc2d5
Connie Rodriguez and Jen Bruce <connie@>
2019-02-27 20:35:10 -0800

    Add initial html, css, js, and looping/filter/map/reverse/boolean

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+
+    var sentence1Input = $("input#sentence1").val();
+    console.log(sentence1Input);
+    // var item2Input = $("input#item2").val();
+    // var item3Input= $("input#item3").val();
+    // var item4Input = $("input#item4").val();
+
+
+
+    var wordArray1 = sentence1Input
+    console.log("wordarr1", wordArray1);
+    alert(typeof wordArray1);
+
+    var wordArray2 = wordArray1.split(" ");
+
+    console.log("word2", wordArray2);
+
+    var wordArray3 = wordArray2.map(function(words) {
+      if (words.length >= 3) {
+        return words;
+      }
+      });// } else if
+      //     words.pop();
+      // }
+      //var filtered = wordArray3.filter(function(words) {
+      //  return true;
+      //});
+      //console.log("filtered", filtered);
+      //console.log(filtered);
+      //var array4 = wordArray3.filter(Number)
+    console.log("word3", wordArray3)
+
+    var wordArray4 = wordArray3.filter(Boolean);
+    console.log("word4", wordArray4)
+
+
+
+    var wordArray5 = wordArray4.reverse()
+    console.log("word5", wordArray5)
+    // var listArray3 = listArray2.sort();
+
+
+    // $(".item1").append(listArray3[0]);
+    // $(".item2").append(listArray3[1]);
+    // $(".item3").append(listArray3[2]);
+    // $(".item4").append(listArray3[3]);
+    //
+    // $(".newSentence").text(listArray3[0]);
+    // $(".this2").text(listArray3[1]);
+    // $(".this3").text(listArray3[2]);
+    // $(".this4").text(listArray3[3]);
+
+
+
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+    // $(".beverage").text(beverageInput);
+    // $(".flavor").text(flavor);
+
+    $("#blanks").hide();
+
+    $("#words").show();
+
+    // $("input:radio[name=flavor]:checked").children("value")first();
+
+  });
+});
